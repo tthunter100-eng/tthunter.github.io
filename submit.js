1a01a43afbe46b936516862c14bc27c4e63c38fb
Damahe1 <daryllmatthewgumapac@gmail.com>
2026-04-09 22:25:53 +0800

    Update submit.js

--- submit.js
+++ submit.js
@@ -13,7 +13,7 @@ personalInfo.innerHTML = `
             <label for="personname" style="font-size: 15px;">Full Name:</label><span style="color: red; font-weight: bold;">*</span><br>
             <input name="full_name" id="person-name" style="height: 20px; width: 100%; background-color: #ffffff; border: none;" required><br><br>
             <label for="personrole" style="font-size: 15px;">Role in School:</label><span style="color: red; font-weight: bold;">*</span><br>
-            <select name="school_role" id="person-role" style="transition: 0.1s ease; height: 30px; min-width: 150px; background-color: transparent; border: 2px solid blue;" required><br><br>
+            <select name="school_role" id="person-role" style="transition: 0.1s ease; height: 30px; min-width: 150px; background-color: transparent; border: none;" required><br><br>
                 <option value="" disabled selected>Select a role</option>
                 <option value="student">Student</option>
                 <option value="staff">Staff</option>
@@ -130,14 +130,14 @@ itemInfo.innerHTML = `
     <div style="box-sizing: border-box; margin-top: 90px; height: 100%; width: 100%; position: relative; display: flex; flex-direction: column; padding: 20px 40px;">
         <form action="placeholder.php" id="iInfo" method="post">
             <label for="itemcode" style="font-size: 15px;">Code:</label><span style="color: red; font-weight: bold;">*</span><br>
-            <select name="item_code" id="item-code" style="transition: 0.1s ease; height: 30px; min-width: 150px; background-color: transparent; border: 2px solid blue;" required>
+            <select name="item_code" id="item-code" style="transition: 0.1s ease; height: 30px; min-width: 150px; background-color: transparent; border: none;" required>
                 <option value="" disabled selected>Select a code</option>
             </select><br><br>
             <label for="itemname" style="font-size: 15px;">Item Name:</label><span id="sym-itemname" style="color: red; font-weight: bold;">/</span><br>
             <input name="item_name" value="N/A" id="item-name" style="height: 20px; width: 100%; background-color: #d1d1d1; border: none;" disabled><br><br>
             <label for="itemdesc" style="font-size: 15px;">Item Description:</label><span id="sym-itemdesc" style="color: red; font-weight: bold;">/</span><br>
             <input name="item_desc" value="N/A" id="item-desc" style="height: 20px; width: 100%; background-color: #d1d1d1; border: none;" disabled><br><br>
-            <label for="features" style="font-size: 15px;">Distinguishing Features (optional):</label><br>
+            <label for="features" style="font-size: 15px;">Distinguishing Features:</label><br>
             <input name="item_features" id="features" style="height: 20px; width: 100%; background-color: #ffffff; border: none;"><br><br>
             <label for="lastknownloc" style="font-size: 15px;">Last Known Location:</label><span style="color: red; font-weight: bold;">*</span><br>
             <input name="last_location" id="last-loc" style="height: 20px; width: 100%; background-color: #ffffff; border: none;" required><br><br>
@@ -210,6 +210,67 @@ itemCode.addEventListener("change", (e) => {
         disabledField(itemDesc, document.getElementById("sym-itemdesc"));
     }
 });
+
+//submit button
+const submitAndCancel = document.createElement("div");
+submitAndCancel.innerHTML = `
+    <input type="checkbox" id="policy-check" style="height: 18px; width: 18px; background-color: transparent; border: 2px solid #898989;" required>
+    <button id="submit-cancel" style="height: 50px; width: 120px; justify-content: center; align-items: center; background-color: #c0c0c0; border: none; cursor: pointer; font-weight: bold; font-size: 15px; color: black; margin-right: 20px;">Cancel</button>
+    <button type="submit" id="submit-ticket" style="height: 50px; width: 120px; justify-content: center; align-items: center; background-color: #2e4279; border: none; cursor: pointer; font-weight: bold; font-size: 15px; color: white; margin-right: 45px;">Submit</button>
+`;
+Object.assign(submitAndCancel.style, {
+    bottom: "40px",
+    height: "60px",
+    width: "100%",
+    position: "absolute",
+    display: "flex",
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+    border: "none",
+    padding: "10px 15px",
+});
+document.querySelector(".div-container").appendChild(submitAndCancel);
+
+//submit button function
+const submitTicket = document.getElementById("submit-ticket");
+submitTicket.onclick = () => {
+    const validatePersonal = validate(pForm);
+    const validateItem = validate(iForm);
+
+    if (validatePersonal && validateItem) {
+        pForm.requestSubmit();
+        iForm.requestSubmit();
+        submitTicket.disabled = true;
+        submitTicket.textContent = "Submitted!";
+    }
+};
+
+//cancel button function
+const cancelButton = document.getElementById("submit-cancel");
+cancelButton.onclick = () => {
+    if(confirm("Are you sure? Your progress will be lost.")) {
+    pForm.reset();
+    iForm.reset();
+    window.location.href = "index.html"
+    }
+};
+
+//required
+function validate(form) {
+    let isValid = true;
+    const requiredFields = form.querySelectorAll("[required]");
+
+    requiredFields.forEach(field => {
+        if (!field.value || field.value.trim() === "") {
+            field.style.border = "2px solid red";
+            isValid = false;
+        }
+        else {
+            field.style.border = "none";
+        }
+    });
+    return isValid;
+}
 
 //item information submission
 const iForm = itemInfo.querySelector("form");
