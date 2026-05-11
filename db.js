0d4d37c85d889509f87bab5daaa42505abb7c153
Damahe1 <daryllmatthewgumapac@gmail.com>
2026-05-12 01:53:14 +0800

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+const client = new MongoClient(uri {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
 
 async function connectToDatabase() {
     try {
