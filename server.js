01fb6fca67b4f5beda1acd177486894b69e632f9
Damahe1 <daryllmatthewgumapac@gmail.com>
2026-05-11 08:26:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectToDatabase = require('./db');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let db;
